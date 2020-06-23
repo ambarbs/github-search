36e1b58573fc128bb03e059cfc5a5a16c248606f
Ambar Bose <ambar.bs@gmail.com>
2020-06-24 08:46:14 +1000

    Updates

---
+++ precache-manifest.8b8e1ff8419f2f8924a94ae19dbc7a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dba2e2c1105119118e7b0faaee506c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdff18ae133005be6c73",
+    "url": "/static/css/main.15a38a67.chunk.css"
+  },
+  {
+    "revision": "b124822559d6f0711a41",
+    "url": "/static/js/2.810dd8fc.chunk.js"
+  },
+  {
+    "revision": "fa4ce2bdceb7bc5a6f480dd7f3fa3051",
+    "url": "/static/js/2.810dd8fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdff18ae133005be6c73",
+    "url": "/static/js/main.a999169e.chunk.js"
+  },
+  {
+    "revision": "bc8b181cb67cb1c31288",
+    "url": "/static/js/runtime-main.6d7287d1.js"
+  }
+]);
