dfcef38330d458fbf2e304f19e824db38b574d32
Ambar Bose <ambar.bs@gmail.com>
2020-06-24 10:44:26 +1000

    segregated search component

--- src/components/app/App.styles.js
+++ src/components/app/App.styles.js
@@ -14,19 +14,4 @@ export const AppWrapper = styled.div`
   justify-content: space-evenly;
   align-items: center;
   min-height: 100vh;
-  //@media (max-width: 576px) {
-  //  flex-direction: column;
-  //}
-`;
-
-export const SearchBox = styled.input`
-  color: #333;
-  background: white;
-  height: 70px;
-  padding: 0 25px;
-  font-size: 1em;
-  border-radius: 6px;
-  margin-bottom: 2rem;
-  margin-top: 2rem;
-  box-shadow: 0 4px 12px rgba(77, 128, 114, 0.3);
 `;
---
+++ src/components/search/Search.js
@@ -0,0 +1,51 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import _ from 'lodash';
+import { GridView } from '../common/Common.styles';
+import Card from '../card/Card';
+import { SearchBox } from './Search.styles';
+
+const Search = () => {
+  const [data, setData] = useState({});
+  const [error, setError] = useState(null);
+  const fetchData = async (url) => {
+    const response = await fetch(url);
+    response
+      .json()
+      .then((res) => setData(res))
+      .catch((err) => setError(err));
+  };
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const debounceLoadData = useCallback(_.debounce(fetchData, 300), []);
+
+  const url = `https://api.github.com/search/repositories?q=${searchTerm}&sort=stars&order=desc`;
+  useEffect(() => {
+    if (searchTerm) {
+      debounceLoadData(url);
+    }
+  }, [url, searchTerm, debounceLoadData]);
+
+  const { items = [] } = data;
+  const renderedContent = error ? (
+    <>Error</>
+  ) : (
+    <GridView>
+      {items.map((item) => {
+        const { id, name, watchers_count: watchersCount } = item;
+        return <Card id={id} name={name} watchersCount={watchersCount} />;
+      })}
+    </GridView>
+  );
+  return (
+    <>
+      <SearchBox
+        placeholder="Search Github repo..."
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
+      {renderedContent}
+    </>
+  );
+};
+
+export default Search;
